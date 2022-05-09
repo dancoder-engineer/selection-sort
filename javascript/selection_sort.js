d88a7683df166a88d0565fb5a2c3251be6c673fb
Dan Seminara <japaneseforgaming@gmail.com>
2022-05-09 14:47:45 -0400

    Done!

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,18 @@
 function selectionSort(arr) {
   // type your code here
+  let sorted = []
+  while (arr.length != 0) {
+    let arrNums = [arr[0], 0]
+    for (let f = 0; f < arr.length; f++) {
+      if (arr[f] < arrNums[0]) { 
+        arrNums[0] = arr[f]
+        arrNums[1] = f
+      }
+    }
+    sorted.push(arr.splice(arrNums[1], 1)[0])
+
+  }
+  return sorted
 }
 
 if (require.main === module) {
